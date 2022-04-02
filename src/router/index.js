7738c7ed7bd222eb63ee882f58ec6f74231bbbb4
Sillfer <gliouinemahdi0@gmail.com>
2022-04-02 18:24:13 +0000

    front-in-progress

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Reference.vue'
+import Signup from '../views/Signup.vue'
+import Rendv from "../views/Rdvs.vue";
+import Reservation from '../views/Reservation.vue'
+
+const routes = [
+  
+  {
+    path: "/rdv/:ref",
+    name: "RendezVous",
+    component: Rendv,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/',
+    name: 'Signup',
+    component: Signup,
+  },
+  {
+    path: '/reservation',
+    name: 'Reservation',
+    component: Reservation,
+  },
+  {
+    path: "/reservation/:ref",
+    name: "Reservation",
+    component: Reservation,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
